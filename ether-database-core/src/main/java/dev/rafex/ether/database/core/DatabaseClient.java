f0f8ef706083a858d181d20d6afd54a1746dacb1
rafex <rafex@rafex.dev>
2026-03-23 01:27:40 -0600

    fix(core) execute

--- ether-database-core/src/main/java/dev/rafex/ether/database/core/DatabaseClient.java
+++ ether-database-core/src/main/java/dev/rafex/ether/database/core/DatabaseClient.java
@@ -17,6 +17,12 @@ public interface DatabaseClient extends TransactionRunner {
 
     <T> Optional<T> queryOne(SqlQuery query, RowMapper<T> mapper);
 
+    /**
+     * Executes a SQL statement (INSERT, UPDATE, DELETE, or stored procedure call).
+     * 
+     * @param query the SQL query to execute
+     * @return the number of rows affected, or -1 if the statement returned a ResultSet
+     */
     int execute(SqlQuery query);
 
     long[] batch(String sql, List<StatementBinder> binders);
